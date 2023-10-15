1142ff588fc795a069e60a246751cd7fdc1f48cb
Ba6uIIIoN <artur12derr@gmail.com>
2023-10-16 00:37:09 +0300

    fgh

--- app/src/main/java/com/example/workfuel/LoginActivity.java
+++ app/src/main/java/com/example/workfuel/LoginActivity.java
@@ -1,61 +1,111 @@
 package com.example.workfuel;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private Button loginButton, registerButton;
+    private Button loginButton;
+    private TextView textViewCreate;
     private Intent intentMain, intentReg;
     private Animation animationScale;
     private SharedPreferences sharedPreferences;
+    private EditText email, password;
+    private ConstraintLayout constraint;
+    private boolean logging;
+    FirebaseAuth auth;
+    FirebaseDatabase db;
+    DatabaseReference users;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         loginButton = findViewById(R.id.loginButton);
-        registerButton = findViewById(R.id.registerButton);
+        textViewCreate = findViewById(R.id.textViewCreate);
+
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseDatabase.getInstance();
+        users = db.getReference("Users");
 
         sharedPreferences = getSharedPreferences("preferences", Context.MODE_PRIVATE);
-        boolean login = sharedPreferences.getBoolean("login", false);
+        boolean logging = sharedPreferences.getBoolean("logging", false);
 
-        // Короче это в конце регистрации
+        textViewCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textViewCreate.setText(R.string.create_underline);
+                intentRegActivity();
+                finish();
 
-        // if (!isRegistered) {
-        //    // Показать окно регистрации
-        //    // Запустить активность регистрации или показать диалоговое окно
-        //} else {
-        // То открываем MainActivity
-        // }
+            }
+        });
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                loginUser();
                 animation();
-                intentMainActivity();
             }
         });
-        registerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                intentRegActivity();
-            }
-        });
+    }
+    private void loginUser() {
+        email = findViewById(R.id.editTextEmail);
+        password = findViewById(R.id.editTextPassword);
+
+        if (TextUtils.isEmpty(email.getText().toString())) {
+            Snackbar.make(constraint, "Вы не ввели почту!", Snackbar.LENGTH_SHORT).show();
+            System.exit(0);
+
+        }
+        if (password.getText().toString().length() < 7) {
+            Snackbar.make(constraint, "Вы не ввели пароль!", Snackbar.LENGTH_SHORT).show();
+            System.exit(0);
+
+        }
+
+        auth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        intentMainActivity();
+                        //Snackbar.make(constraint, "Вы успешно вошли в аккаунт!", Snackbar.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        //Snackbar.make(constraint, "Упс! Что-то пошло не так..." + e.getMessage(), Snackbar.LENGTH_SHORT).show();
+                    }
+                });
+
+
     }
     private void animation() {
         animationScale = AnimationUtils.loadAnimation(this, R.anim.animation_button_scale);
     }
     private void intentMainActivity() {
-        intentMain = new Intent(this, MainActivity.class);
+        intentMain = new Intent(this, MenuActivity.class);
         startActivity(intentMain);
     }
     private void intentRegActivity() {
@@ -66,9 +116,6 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        // Либо в конце регистрации (что более вероятно)
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putBoolean("isRegistered", true);
-        editor.apply();
+
     }
 }
--- app/src/main/java/com/example/workfuel/RegistrationActivity.java
+++ app/src/main/java/com/example/workfuel/RegistrationActivity.java
@@ -1,50 +1,150 @@
 package com.example.workfuel;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.workfuel.objects.User;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegistrationActivity extends AppCompatActivity {
 
     private Button regButton, backLoginButton;
-    private Intent intentMain;
+    private TextView textViewLogin;
+    private Intent intentMain, intentReg;
     private Animation animationScale;
+    private EditText name, email, password, passwordRepeat;
+    private ConstraintLayout constraint;
+    private SharedPreferences sharedPreferences;
+    private boolean register;
+    FirebaseAuth auth;
+    FirebaseDatabase db;
+    DatabaseReference users;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        constraint = findViewById(R.id.constraint);
         regButton = findViewById(R.id.regButton);
-        backLoginButton = findViewById(R.id.backLoginButton);
+        textViewLogin = findViewById(R.id.textViewLogin);
+
+        sharedPreferences = getSharedPreferences("preferences", Context.MODE_PRIVATE);
+        boolean register = sharedPreferences.getBoolean("register", false);
+
+        // авторизируем бд
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseDatabase.getInstance();
+        users = db.getReference("Users");
 
         regButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                registrationUser();
                 animation();
-                intentMainActivity();
+
             }
         });
-        backLoginButton.setOnClickListener(new View.OnClickListener() {
+        textViewLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                textViewLogin.setText(R.string.login_underline);
                 intentBackActivity();
             }
         });
+    }
+    private void registrationUser() {
+        name = findViewById(R.id.editTextName);
+        email = findViewById(R.id.editTextEmail);
+        password = findViewById(R.id.editTextPassword);
+        passwordRepeat = findViewById(R.id.editTextPasswordRepeat);
+
+        if (TextUtils.isEmpty(name.getText().toString())) {
+            Snackbar.make(constraint, "Вы не ввели имя!", Snackbar.LENGTH_SHORT).show();
+            System.exit(0);
+
+        }
+        if (TextUtils.isEmpty(email.getText().toString())) {
+            Snackbar.make(constraint, "Вы не ввели почту!", Snackbar.LENGTH_SHORT).show();
+            System.exit(0);
+
+        }
+        if (password.getText().toString().length() < 7) {
+            Snackbar.make(constraint, "Вы не ввели пароль!", Snackbar.LENGTH_SHORT).show();
+            System.exit(0);
+
+        }
+        if (passwordRepeat.getText().toString().equals(password.getText().toString())) {
+            Snackbar.make(constraint, "Пароли не совпадают!", Snackbar.LENGTH_SHORT).show();
+            System.exit(0);
+
+        }
+        // рег
+        auth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        // Создание объекта
+                        User user = new User();
+                        // Устанавливаем значения
+                        user.setEmail(email.getText().toString());
+                        user.setName(name.getText().toString());
+                        user.setPassword(password.getText().toString());
+
+                        //Идентификация пользователи
+                        users.child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                .setValue(user)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        intentMainActivity();
+                                        //Snackbar.make(constraint, "Аккаунт успешно зарегистрирован!", Snackbar.LENGTH_SHORT).show();
+
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        //Snackbar.make(constraint, "Упс! Что-то пошло не так..." + e.getMessage(), Snackbar.LENGTH_SHORT).show();
+                                    }
+                                });
+
+
+                    }
+                });
     }
     private void animation() {
         animationScale = AnimationUtils.loadAnimation(this, R.anim.animation_button_scale);
     }
     private void intentMainActivity() {
-        intentMain = new Intent(this, MainActivity.class);
+        intentMain = new Intent(this, MenuActivity.class);
         startActivity(intentMain);
     }
     private void intentBackActivity() {
         finish();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 
 }
